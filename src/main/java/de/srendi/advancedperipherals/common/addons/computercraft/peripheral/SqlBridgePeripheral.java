1232743b6092342618e8a9b850b84b4952602c81
DESKTOP-4DV71MT\rober <gordododelphi@gmail.com>
2021-03-04 15:28:39 -0300

    small updates

--- src/main/java/de/srendi/advancedperipherals/common/addons/computercraft/peripheral/SqlBridgePeripheral.java
+++ src/main/java/de/srendi/advancedperipherals/common/addons/computercraft/peripheral/SqlBridgePeripheral.java
@@ -1,6 +1,8 @@
 package de.srendi.advancedperipherals.common.addons.computercraft.peripheral;
 
 
+import dan200.computercraft.api.lua.LuaFunction;
+import de.srendi.advancedperipherals.common.addons.sqlbridge.ConnectionFactory;
 import de.srendi.advancedperipherals.common.blocks.base.PeripheralTileEntity;
 import de.srendi.advancedperipherals.common.configuration.AdvancedPeripheralsConfig;
 import net.minecraft.tileentity.TileEntity;
@@ -21,5 +23,13 @@ public class SqlBridgePeripheral extends BasePeripheral {
     @Override
     public boolean isEnabled() {
         return AdvancedPeripheralsConfig.enableSqlBridge;
+    }
+    @LuaFunction(mainThread = true)
+    public final boolean Connect(String data_url,String username, String password){
+ConnectionFactory factory_db = new ConnectionFactory();
+factory_db.setDatabase_url(data_url);
+factory_db.setPassword(password);
+factory_db.setUsername(username);
+
     }
 }
---
+++ src/main/java/de/srendi/advancedperipherals/common/addons/sqlbridge/ConnectionFactory.java
@@ -0,0 +1,44 @@
+package de.srendi.advancedperipherals.common.addons.sqlbridge;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    private  String username;
+    private  String password;
+    private  String database_url;
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDatabase_url() {
+        return database_url;
+    }
+
+    public void setDatabase_url(String database_url) {
+        this.database_url = database_url;
+    }
+
+public Connection createConnectionToMySql() throws ClassNotFoundException, SQLException {
+    Class.forName("com.mysql.cj.jdbc.Driver");
+Connection connection = DriverManager.getConnection(this.database_url,this.username,this.password);
+return connection;
+}
+
+
+}
+
+
